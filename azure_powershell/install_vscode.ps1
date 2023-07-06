7f232fc7c89fdc9d018d444524c3dc0868fcaea6
Adhi-AWS <90025213+Adhi-AWS@users.noreply.github.com>
2023-07-06 17:31:19 +0530

    Update install_vscode.ps1

--- azure_powershell/install_vscode.ps1
+++ azure_powershell/install_vscode.ps1
@@ -30,17 +30,7 @@ Write-Host "Visual Studio Code has been installed."
 
 
 
-# # Download VS Code installer
-# $installerUrl = "https://aka.ms/win32-x64-user-stable"
-# $installerPath = "$env:TEMP\vscode-installer.exe"
-# Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
 
-# # Start the installer silently
-# $process = Start-Process -FilePath $installerPath -ArgumentList "/silent" -PassThru
-# $process.WaitForExit()
-
-# # Clean up the installer
-# Remove-Item -Path $installerPath
 
 
 
