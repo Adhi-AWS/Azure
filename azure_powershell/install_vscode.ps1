d3d75d02fc652808dc182277d068aae6d968b367
Adhi-AWS <90025213+Adhi-AWS@users.noreply.github.com>
2023-07-06 14:20:59 +0530

    Update install_vscode.ps1

--- azure_powershell/install_vscode.ps1
+++ azure_powershell/install_vscode.ps1
@@ -1,12 +1,47 @@
-# Download VS Code installer
-$installerUrl = "https://aka.ms/win32-x64-user-stable"
-$installerPath = "$env:TEMP\vscode-installer.exe"
-Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+$downloadUrl = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
+$installDir = "C:\Program Files\VSCode"
+$arguments = "/VERYSILENT /MERGETASKS=!runcode"
 
-# Start the installer silently
-$process = Start-Process -FilePath $installerPath -ArgumentList "/silent" -PassThru
-$process.WaitForExit()
+$installerPath = "$env:TEMP\VSCodeSetup.exe"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
 
-# Clean up the installer
-Remove-Item -Path $installerPath
+$installCommand = "$arguments" + ' /DIR="' + $installDir + '"'
+Start-Process -FilePath $installerPath -ArgumentList $installCommand -Wait
+
+Write-Host "Visual Studio Code has been installed."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Download VS Code installer
+# $installerUrl = "https://aka.ms/win32-x64-user-stable"
+# $installerPath = "$env:TEMP\vscode-installer.exe"
+# Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+
+# # Start the installer silently
+# $process = Start-Process -FilePath $installerPath -ArgumentList "/silent" -PassThru
+# $process.WaitForExit()
+
+# # Clean up the installer
+# Remove-Item -Path $installerPath
+
+
+
 
